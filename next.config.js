79e11efb66f8cddec7692a5d65681ac7474a2f03
ethan-t-hansen <ethan.t.hansen@gmail.com>
2023-07-02 15:48:29 -0700

    config changes, css fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const withImage = require('next-images')
+
 const nextConfig = {
     output: 'export',
 }
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    withImage
+}
